04aeee5dde5858c0c87570009f0818d55e8b948c
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2024-05-14 14:11:51 +0100

    gallery markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,24 @@
 // eslint-disable-next-line import/extensions
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+let markup = '';
+galleryItems.forEach(({ preview, original, description }) => {
+    // console.log(item.preview);
+    const content = /* html */ `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</li>`;
+    markup += content;
+});
+console.log(markup);
+gallery.insertAdjacentHTML('beforeend', markup);
