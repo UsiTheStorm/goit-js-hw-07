889e72da60d3813f231fe21e4a13882983b6eb52
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2024-05-14 14:41:57 +0100

    delegation emplement

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,10 +5,10 @@ console.log(galleryItems);
 
 const gallery = document.querySelector('.gallery');
 
-let markup = '';
-galleryItems.forEach(({ preview, original, description }) => {
-    // console.log(item.preview);
-    const content = /* html */ `<li class="gallery__item">
+function createMarkup() {
+    let markup = '';
+    galleryItems.forEach(({ preview, original, description }) => {
+        const content = /* html */ `<li class="gallery__item">
   <a class="gallery__link" href="${original}">
   <img
     class="gallery__image"
@@ -18,7 +18,24 @@ galleryItems.forEach(({ preview, original, description }) => {
   />
 </a>
 </li>`;
-    markup += content;
-});
-console.log(markup);
-gallery.insertAdjacentHTML('beforeend', markup);
+        markup += content;
+    });
+    console.log(markup);
+    gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+function onImageClick(event) {
+    event.preventDefault();
+
+    const { target } = event;
+    if (target.nodeName !== 'IMG') {
+        console.log('мимo');
+        return;
+    }
+    console.log('okay');
+    const originalLink = target.dataset.source;
+    console.log(originalLink);
+}
+
+createMarkup();
+gallery.addEventListener('click', onImageClick);
