a70e9f17ab68f2def2de0ac22b1bee0c8467af8c
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2024-05-15 15:35:04 +0100

    implementation of modal

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
+// eslint-disable-next-line import/extensions
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+function createMarkup() {
+    let markup = '';
+    galleryItems.forEach(({ preview, original, description }) => {
+        const content = /* html */ `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+   </li>`;
+        markup += content;
+    });
+    gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+createMarkup();
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+});
+console.log(lightbox);
